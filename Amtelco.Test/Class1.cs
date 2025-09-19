52ec3101d153bb782d805fc20a910c229290268a
rjalbert <rjalbert@amtelco.com>
2025-09-19 10:41:03 -0500

    - adding github action

--- Amtelco.Test/Class1.cs
+++ Amtelco.Test/Class1.cs
@@ -8,5 +8,15 @@ namespace Amtelco.Test
 {
     public class Class1
     {
+        public async Task DoStuff()
+        {
+            List<Task> pending = new List<Task>();
+            Task timeoutTask = Task.Delay(30000);
+            Task first = await Task.WhenAny(Task.WhenAll(pending.ToArray()), timeoutTask);
+            if(first == timeoutTask)
+                throw new TimeoutException("Sending data to Active Insights");
+            await first;    // handle any exceptions from pending
+
+        }
     }
 }
