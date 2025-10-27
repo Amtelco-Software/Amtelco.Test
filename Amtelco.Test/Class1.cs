cc8cba0ad0f8444765a892d4ba12ff3b8f8434d2
rjalbert <rjalbert@amtelco.com>
2025-10-27 16:09:16 -0500

    - added method that should pass code scanning

--- Amtelco.Test/Class1.cs
+++ Amtelco.Test/Class1.cs
@@ -18,5 +18,9 @@ namespace Amtelco.Test
             await first;    // handle any exceptions from pending
 
         }
+        public static void DoMoreStuff()
+        {
+            Console.WriteLine("DoMoreStuff");
+        }
     }
 }
